07ff11d94e7a64ddb3eeec637c79ae392cbf00db
Jamie McCrindle <jamiemccrindle@gmail.com>
2015-04-05 22:00:06 +0100

    just return OK

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"log"
-	"io/ioutil"
 )
 
 func main() {
@@ -16,34 +14,8 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-			w.Header().Add("Cache-Control", "public, max-age=600")
-			w.Header().Add("Access-Control-Allow-Origin", "*")
-			w.Header().Add("Access-Control-Max-Age", "600")
-			w.Header().Add("Access-Control-Allow-Headers", "accept, custom")
-			w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, POST, DELETE")
-
-			if r.Method == "OPTIONS" {
-
-				log.Println("OPTIONS")
-
-				w.WriteHeader(200)
-				fmt.Fprintf(w, "OK")
-
-			} else {
-
-				log.Println(r.URL)
-				body, err := ioutil.ReadAll(r.Body);
-
-				if err != nil {
-					log.Println(err)
-				}
-
-				log.Println(string(body))
-
-				w.WriteHeader(200)
-				fmt.Fprintf(w, "OK")
-
-			}
+			w.WriteHeader(200)
+			fmt.Fprintf(w, "OK")
 
 		})
 
